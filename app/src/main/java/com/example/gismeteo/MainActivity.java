e15e896da703d3882a88666342dd43b26d20acf1
vituhaa <vbuzina72@gmail.com>
2026-02-27 17:01:46 +0300

    browser chenging added

--- app/src/main/java/com/example/gismeteo/MainActivity.java
+++ app/src/main/java/com/example/gismeteo/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.gismeteo;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +14,34 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                PackageManager manager = getPackageManager();
+                Intent yandexIntent = manager.getLaunchIntentForPackage("com.yandex.browser1");
+                Intent chromeIntent = manager.getLaunchIntentForPackage("com.android.chrome1");
+                if (yandexIntent != null)
+                {
+                    startActivity(yandexIntent);
+                }
+                else if (chromeIntent != null)
+                {
+                    startActivity(chromeIntent);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "No app", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

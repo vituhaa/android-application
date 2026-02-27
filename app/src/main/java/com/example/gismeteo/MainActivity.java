606e909334d50ee9d90a5e6904ca1c07c238144d
vituhaa <vbuzina72@gmail.com>
2026-02-28 00:17:16 +0300

    url paths added

--- app/src/main/java/com/example/gismeteo/MainActivity.java
+++ app/src/main/java/com/example/gismeteo/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.gismeteo;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -36,8 +39,69 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox time4;
     private List<CheckBox> city_arr;
     private List<CheckBox> time_arr;
+    private ArrayList<CheckBox> choice_arr;
 
-    
+    public String make_url(ArrayList<CheckBox> choice)
+    {
+        String city_url;
+        String time_url;
+        String final_url;
+        CheckBox city_idx = choice.get(0);
+        CheckBox time_idx = choice.get(1);
+        String city = city_idx.getText().toString();
+        String time = time_idx.getText().toString();
+//        Toast.makeText(getApplicationContext(), city, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), time, Toast.LENGTH_SHORT).show();
+
+        if (city.equals("Nizhny Novgorod"))
+        {
+            city_url = "nizhny-novgorod";
+        }
+        else if (city.equals("Uren"))
+        {
+            city_url = "uren";
+        }
+        else if (city.equals("Zavolgie"))
+        {
+            city_url = "zavoljie";
+        }
+        else if (city.equals("Kronshtadt"))
+        {
+            city_url = "kronstadt";
+        }
+        else if (city.equals("Yoshkar-Ola"))
+        {
+            city_url = "yoshkar-ola";
+        }
+        else
+        {
+            city_url = "yaroslavl";
+        }
+
+
+        if (time.equals("Today"))
+        {
+            time_url = "today";
+        }
+        else if (time.equals("Tomorrow"))
+        {
+            time_url = "tomorrow";
+        }
+        else if (time.equals("3 days"))
+        {
+            time_url = "3-day-weather";
+        }
+        else
+        {
+            time_url = "10-day-weather";
+        }
+
+        final_url = "https://yandex.ru/pogoda/ru/" + city_url + "/details/" + time_url;
+
+        // url format:  https://yandex.ru/pogoda/ru/nizhny-novgorod/details/today
+        return final_url;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,16 +132,103 @@ public class MainActivity extends AppCompatActivity {
         time_arr.add(time2);
         time_arr.add(time3);
         time_arr.add(time4);
+        choice_arr = new ArrayList<CheckBox>();
         button3 = (Button)findViewById(R.id.button3);
+
+
+        for (CheckBox cheker : city_arr) {
+            cheker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+//                    for (int i = 0; i < city_arr.size(); i++)
+//                    {
+//                        if (city_arr.get(i).isChecked())
+//                        {
+//                            choice_arr.add(city_arr.get(i));
+//                            for (int k = 0; k < city_arr.size(); k++)
+//                            {
+//                                if (k != i)
+//                                {
+//                                    city_arr.get(k).setEnabled(false);
+//                                }
+//                            }
+//                            break;
+//                        }
+//                    }
+                    if (isChecked)
+                    {
+                        for (CheckBox check : city_arr)
+                        {
+                            if (check != buttonView)
+                            {
+                                check.setEnabled(false);
+                            }
+                            else
+                            {
+                                choice_arr.add(check);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (CheckBox check : city_arr)
+                        {
+                            choice_arr.remove(check);
+                            check.setEnabled(true);
+                        }
+                    }
+                }
+            });
+        }
+
+        for (CheckBox cheker : time_arr) {
+            cheker.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
+//                    for (int j = 0; j < time_arr.size(); j++)
+//                    {
+//                        if (time_arr.get(j).isChecked())
+//                        {
+//                            choice_arr.add(time_arr.get(j));
+//                            for (int l = 0; l < time_arr.size(); l++)
+//                            {
+//                                if (l != j)
+//                                {
+//                                    time_arr.get(l).setEnabled(false);
+//                                }
+//                            }
+//                            break;
+//                        }
+//                    }
+                    if (isChecked)
+                    {
+                        for (CheckBox check : time_arr)
+                        {
+                            if (check != buttonView)
+                            {
+                                check.setEnabled(false);
+                            }
+                            else
+                            {
+                                choice_arr.add(check);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (CheckBox check : time_arr)
+                        {
+                            choice_arr.remove(check);
+                            check.setEnabled(true);
+                        }
+                    }
+                }
+            });
+        }
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
-//                if (city1.isChecked())
-//                {
-//                    Toast.makeText(MainActivity.this, "Выбран город", Toast.LENGTH_SHORT).show();
-//
-//                }
                 int count = 0;
                 for (int i = 0; i < city_arr.size(); i++)
                 {
@@ -104,9 +255,14 @@ public class MainActivity extends AppCompatActivity {
                     PackageManager manager = getPackageManager();
                     Intent yandexIntent = manager.getLaunchIntentForPackage("com.yandex.browser");
                     Intent chromeIntent = manager.getLaunchIntentForPackage("com.android.chrome");
+                    String url = make_url(choice_arr);
+                    Intent weather_url = new Intent(Intent.ACTION_VIEW);
+                    weather_url.setData(Uri.parse(url));
+                    weather_url.addCategory(Intent.CATEGORY_BROWSABLE);
                     if (yandexIntent != null)
                     {
                         startActivity(yandexIntent);
+                        startActivity(weather_url);
                     }
                     else if (chromeIntent != null)
                     {
